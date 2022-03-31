92f2b664ccbc633ab4850a8451f706f7eccc98a6
Emre Çakıral <emrecakiral@outlook.com>
2022-03-31 19:21:03 +0300

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 78);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(208, 269);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 18);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(292, 50);
+            this.txtInput.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(231, 161);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(88, 166);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Red;
+            this.btnClear.Location = new System.Drawing.Point(231, 78);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(62, 36);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "<-";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.Black;
+            this.btnC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnC.Location = new System.Drawing.Point(231, 122);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(62, 31);
+            this.btnC.TabIndex = 4;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 350);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hesap Makinesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TextBox txtInput;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Button btnClear;
+        private Button btnC;
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,182 @@
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> nums = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "0", "=" };
+
+        List<string> operatos = new List<string>() { "+", "-", "*", "/" };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txtInput.Text = "0";
+            foreach (var num in nums)
+            {
+                Button btn = new Button();
+                btn.Text = num;
+                btn.Width = 60;
+                btn.Height = 50;
+
+                if (num != "=")
+                    btn.Click += Btn_Click;
+                else
+                    btn.Click += Hesapla_Click;
+
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+
+            foreach (var opt in operatos)
+            {
+                Button btn = new Button();
+                btn.Text = opt;
+                btn.Width = 60;
+                btn.Margin = new Padding(0, 1, 0, 0);
+                btn.Height = 35;
+                btn.Click += Btn_Click1;
+                btn.Enabled = false;
+                flowLayoutPanel2.Controls.Add(btn);
+            }
+        }
+
+        string Hesapla()
+        {
+            string[] str = txtInput.Text.Split(islem);
+            if (txtInput.Text.IndexOf(islem) > 0)
+            {
+                double sayi1 = 0, sayi2 = 0, sonuc = 0;
+                sayi1 = Convert.ToDouble(str[0]);
+                try
+                {
+                    sayi2 = Convert.ToDouble(str[1]);
+                }
+                catch { }
+                switch (islem)
+                {
+                    case "+":
+                        sonuc = sayi1 + sayi2;
+                        break;
+                    case "*":
+                        sonuc = sayi1 * sayi2;
+                        break;
+                    case "/":
+                        sonuc = sayi1 / sayi2;
+                        break;
+                    case "-":
+                        sonuc = sayi1 - sayi2;
+                        break;
+                }
+                return sonuc.ToString();
+                //DataTable dt = new DataTable();
+                //object result = dt.Compute(txtInput.Text, "");
+                //return result.ToString();
+            }
+            else
+            {
+                return txtInput.Text;
+            }
+        }
+
+        private void Hesapla_Click(object? sender, EventArgs e)
+        {
+            //DataTable dt = new DataTable();
+            //object result = dt.Compute(txtInput.Text, "");
+            //txtInput.Text = result.ToString();
+            txtInput.Text = Hesapla();
+        }
+
+        string islem = "";
+        private void Btn_Click1(object? sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            // DisableOperatorsButton(false);
+
+            // son karakter
+            string str = txtInput.Text[txtInput.Text.Length - 1].ToString();
+            if (operatos.IndexOf(str) > -1) // operatör karakteri ise
+            {
+                txtInput.Text = txtInput.Text.Replace(str, "");
+            }
+
+            string t = Hesapla();
+            if (t != "a")
+            {
+                txtInput.Text = t + btn.Text;
+            }
+            else
+            {
+                txtInput.Text += btn.Text;
+            }
+            islem = btn.Text;
+        }
+
+        // e parametresi => eventi temsil eder..
+        // sender => metodu tetikleyen kontrolü temsil eder...
+        // buttonlarýmýzýn referansý sender parametresi ile Btn_Click metoduna gönderilir..Bu referans object tipinde olduðu için bunu unboxing yöntemiyle btn nesnesine atarýz..Artýk referansda gönderilen button bilgilerine btn nesnesine ile eriþiriz...
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            txtInput.Text += btn.Text;
+            virgulEngelle();
+            DisableOperatorsButton(true);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Show metodu dönüþ tipi DialogResulttýr. Dialog buttonlardan hangisine týklanrýrsa o tipte result döner..
+            DialogResult result = MessageBox.Show("Uygulama Kapanacak. \nEmin misin?", "Uyarý", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Cancel) // result cancel ise
+            {
+                e.Cancel = true; // formu kapatma....
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            // tek tek temizle
+            if (txtInput.Text.Length > 0)
+                txtInput.Text = txtInput.Text.Remove(txtInput.Text.Length - 1, 1);
+
+
+
+            // eðer text silindiyse
+            if (txtInput.Text.Length == 0)
+                DisableOperatorsButton(false); // iþlem operatörleri disable et...
+        }
+
+        void DisableOperatorsButton(bool isEnable)
+        {
+            // Control
+            foreach (Control control in flowLayoutPanel2.Controls)
+            {
+                control.Enabled = isEnable;
+            }
+        }
+
+        void virgulEngelle()
+        {
+            int index = txtInput.Text.IndexOf(",");
+            if (index == 0)
+            {
+                txtInput.Text = txtInput.Text.Insert(0, "0");
+            }
+            else if (index > 0)
+            {
+                index = txtInput.Text.IndexOf(",", index + 1);
+                if (index > 0)
+                {
+                    txtInput.Text = txtInput.Text.Remove(index, 1);
+                }
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            DisableOperatorsButton(false);
+        }
+    }
+}
