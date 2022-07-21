b4a250ccfc6d13b9ec1c3e3fed2f1952cdd2a565
Emre Çakıral <emrecakiral@outlook.com>
2022-07-21 12:40:15 +0300

    Some bugs fixed.

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -37,33 +37,33 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 78);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(11, 67);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(208, 269);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(203, 245);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(12, 18);
-            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInput.Location = new System.Drawing.Point(11, 16);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(292, 50);
+            this.txtInput.Size = new System.Drawing.Size(274, 44);
             this.txtInput.TabIndex = 1;
             // 
             // flowLayoutPanel2
             // 
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(231, 161);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(222, 144);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(88, 166);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(78, 144);
             this.flowLayoutPanel2.TabIndex = 2;
             // 
             // btnClear
             // 
             this.btnClear.BackColor = System.Drawing.Color.Red;
-            this.btnClear.Location = new System.Drawing.Point(231, 78);
+            this.btnClear.Location = new System.Drawing.Point(222, 67);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(62, 36);
+            this.btnClear.Size = new System.Drawing.Size(63, 39);
             this.btnClear.TabIndex = 3;
             this.btnClear.Text = "<-";
             this.btnClear.UseVisualStyleBackColor = false;
@@ -73,9 +73,9 @@
             // 
             this.btnC.BackColor = System.Drawing.Color.Black;
             this.btnC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnC.Location = new System.Drawing.Point(231, 122);
+            this.btnC.Location = new System.Drawing.Point(222, 101);
             this.btnC.Name = "btnC";
-            this.btnC.Size = new System.Drawing.Size(62, 31);
+            this.btnC.Size = new System.Drawing.Size(63, 37);
             this.btnC.TabIndex = 4;
             this.btnC.Text = "C";
             this.btnC.UseVisualStyleBackColor = false;
@@ -83,14 +83,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 350);
+            this.ClientSize = new System.Drawing.Size(312, 304);
             this.Controls.Add(this.btnC);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hesap Makinesi";
